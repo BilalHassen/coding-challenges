aed56e83a635595a4e129add5af267927b826c4a
BilalHassen <bilalhassen097@gmail.com>
2025-01-17 10:04:25 -0500

    solution for missingChar

---
+++ warmup/missingChar.js
@@ -0,0 +1,13 @@
+/*
+Warmup-1 -- missingChar
+Given a non-empty string and an int n, 
+return a new string where the char at index n has been removed. 
+The value of n will be a valid index of a char in the original string 
+(i.e. n will be in the range 0..str.length()-1 inclusive 
+*/
+
+function missingChar(str, n) {
+  return str.slice(0, n) + str.slice(n + 1, str.length);
+}
+
+console.log(missingChar("chocolate", 3));
