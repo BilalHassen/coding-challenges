ac6458cc209f8dca608d9bb057fc5bda1025778a
BilalHassen <bilalhassen097@gmail.com>
2025-01-19 13:47:31 -0500

    solution for backAround.js

---
+++ warmup/backAround.js
@@ -0,0 +1,12 @@
+/*
+Warmup-1 -- backAround
+Given a string, take the last char and return a new string 
+with the last char added at the front and back, so 'cat' yields 'tcatt' 
+The original string will be length 1 or more.
+*/
+
+function backAround(str) {
+  return str.charAt(str.length - 1) + str + str.charAt(str.length - 1);
+}
+
+console.log(backAround("Hassen"));
