b8b5fb380d214784dca2006092df9200a226527a
BilalHassen <bilalhassen097@gmail.com>
2025-01-22 11:49:27 -0500

    solution for in1020

---
+++ warmup/in1020.js
@@ -0,0 +1,8 @@
+/*
+Warmup-1 -- in1020
+Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+*/
+
+function in1020(a, b) {
+  return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
+}
