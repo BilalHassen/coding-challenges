f666c7dc176648b30429feef3bf9df052357230c
Bilal Hassen <bilalhassen097@gmail.com>
2025-04-06 15:36:51 -0400

    solution for countXX

---
+++ warmup-2/countXX.js
@@ -0,0 +1,17 @@
+/*
+Warmup-2 -- countXX
+Count the number of 'xx' in the given string. 
+We'll say that overlapping is allowed, so 'xxx' contains 2 'xx'.
+*/
+
+function countXX(str){
+    let count = 0;
+    for (let i = 0; i < str.length - 1; i++) {
+      if (str[i] === 'x' && str[i + 1] === 'x') {
+        count++;
+      }
+    }
+    return count;
+}
+
+console.log(countXX("strxx"))
